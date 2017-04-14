5c6cd4781a726f227b521aeb1099fad1270611ad
christian kewerkopf <christiankewerkopf@gmail.com>
2017-04-14 13:46:11 +0200

    corrections, variable rolename + templatedir

--- ansibletemplate.go
+++ ansibletemplate.go
@@ -2,63 +2,46 @@ package main
 
 import "os"
 import "fmt"
-import "path/filepath"
 
-var rolename string = "testrole1"
-var text string = "Creating Ansible-Test-kitchen template for"
-var installdir string = "/Users/christiankewerkopf/cinq/git/go/src/ansibletemplate/"
-var kitchenyml string = installdir + rolename + "/.kitchen.yml"
-var templatedir string = installdir + rolename + "/"
+import flag "github.com/ogier/pflag"
 
-func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+var rolename string
+var templatedir string
+var installdir string
+
+func init() {
+	flag.StringVarP(&rolename, "rolename", "r", "fucknut", "help message for flagname")
+	flag.StringVarP(&installdir, "installdir", "i", "./", "help message for flagname")
+	flag.Parse()
 }
 
 func main() {
-{
-os.Mkdir(templatedir, os.FileMode(511))
-os.Mkdir(templatedir + "files", os.FileMode(511))
-os.Mkdir(templatedir + "defaults", os.FileMode(511))
-os.Mkdir(templatedir + "handlers", os.FileMode(511))
-os.Mkdir(templatedir + "test", os.FileMode(511))
-os.Mkdir(templatedir + "meta", os.FileMode(511))
-os.Mkdir(templatedir + "tasks", os.FileMode(511))
-os.Mkdir(templatedir + "templates", os.FileMode(511))
-os.Mkdir(templatedir + "vars", os.FileMode(511))
-os.Mkdir(templatedir + "test/integration", os.FileMode(511))
-os.Mkdir(templatedir + "test/integration/" + rolename, os.FileMode(511))
-os.Mkdir(templatedir + "test/integration/spec", os.FileMode(511))
-os.Mkdir(templatedir, os.FileMode(511))
+	templatedir = fmt.Sprintf("%s/%s/", installdir, rolename)
+
+	CreateTemplateDir(templatedir)
+	CreateTemplateDir(templatedir + "files")
+	CreateTemplateDir(templatedir + "defaults")
+	CreateTemplateDir(templatedir + "handlers")
+	CreateTemplateDir(templatedir + "test")
+	CreateTemplateDir(templatedir + "meta")
+	CreateTemplateDir(templatedir + "tasks")
+	CreateTemplateDir(templatedir + "templates")
+	CreateTemplateDir(templatedir + "vars")
+	CreateTemplateDir(templatedir + "test/integration")
+	CreateTemplateDir(templatedir + "test/integration/" + rolename)
+	CreateTemplateDir(templatedir + "test/integration/spec")
+
+	os.Create(templatedir + "/defaults/main.yml")
+	os.Create(templatedir + "/handlers/main.yml")
+	os.Create(templatedir + "/meta/main.yml")
+	os.Create(templatedir + "/tasks/main.yml")
+	os.Create(templatedir + "/vars/main.yml")
+	os.Create(templatedir + "/test/integration/spec/" + rolename + "_spec.rb")
+	os.Create(templatedir + "/test/integration/" + rolename + "/default.yml")
 }
 
-{
-os.Create(templatedir + "/defaults/main.yml")
-os.Create(templatedir + "/handlers/main.yml")
-os.Create(templatedir + "/meta/main.yml")
-os.Create(templatedir + "/tasks/main.yml")
-os.Create(templatedir + "/vars/main.yml")
-os.Create(templatedir + "/test/integration/spec/rolename_spec.rb")
-os.Create(templatedir + "/test/integration/" + rolename +"/default.yml")
-}
-
-{
-err :=  os.Link(installdir + ".kitchen.yml", kitchenyml)
-
-      if err != nil {
-           fmt.Println(err)
-           return
-}
-}
-
-{
-fmt.Println(text + " " + rolename)
-}
-
-{
-files, _ := filepath.Glob(installdir + rolename + "/*")
-fmt.Println("created",files)
-}
-
+//CreateTemplateDir creates a directory
+func CreateTemplateDir(n string) {
+	os.Mkdir(n, os.FileMode(511))
+	fmt.Printf("created directory %s with filemode 511\n", n)
 }
